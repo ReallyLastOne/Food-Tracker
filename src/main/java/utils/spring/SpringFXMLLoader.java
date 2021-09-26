6c4a83f96c962d4c8ccbd91a2af14a4851274ce7
ReallyLastOne <87190556+ReallyLastOne@users.noreply.github.com>
2021-09-26 13:10:34 +0200

    Update SpringFXMLLoader.java
    Delete comment and unused code.

--- src/main/java/utils/spring/SpringFXMLLoader.java
+++ src/main/java/utils/spring/SpringFXMLLoader.java
@@ -19,38 +19,11 @@ public class SpringFXMLLoader {
         loader.setControllerFactory(applicationContext::getBean);
         loader.setLocation(getClass().getResource(url));
 
-        //loader.setResources(ResourceBundle.getBundle(resources));
         try {
             return loader.load();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public Parent loadFXML(String fxmlLocation)
-            throws IOException {
-
-        //FXMLLoader loader = new FXMLLoader(fxmlLocation);
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlLocation));
-        // load controllers from application context:
-        loader.setControllerFactory(applicationContext::getBean);
-
-        // load controls from application context, where available:
-        loader.setBuilderFactory(new BuilderFactory() {
-            JavaFXBuilderFactory defaultFactory = new JavaFXBuilderFactory();
-
-            @Override
-            public javafx.util.Builder<?> getBuilder(Class<?> type) {
-                String[] beanNames = applicationContext.getBeanNamesForType(type);
-                if (beanNames.length == 1) {
-                    return (Builder<Object>) () -> applicationContext.getBean(beanNames[0]);
-                } else {
-                    return defaultFactory.getBuilder(type) ;
-                }
-            }
-        });
-
-        return loader.load();
     }
 }
